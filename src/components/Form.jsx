efe822df8514c20a0ef2d6cfecde73ad1145899a
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-18 14:42:14 +0100

    Add/Load memeData and style components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,31 @@
+import memes from '../memesData'
+
 export default function Form() {
+
+    function getMeme() {
+        const data = memes.data.memes
+        const randomNumber = Math.floor(Math.random() * data.length)
+        const url = data[randomNumber].url
+        console.log(url)
+    }
+
     return(
         <main className="section">
-            <form>
+
+            <div>
                 <div className="input">
-                    <input type="text" className="text" />
-                    <input type="text" className="text"/>
+                    <input type="text" className="text" placeholder="Enter top text" />
+                    <input type="text" className="text" placeholder="Enter buttom text" />
                 </div>
                 <div className="button_container">
-                    <button className="button">Get a new meme image</button>
+                    <button className="button" onClick={getMeme}>
+                        Get a new meme image 🖼 
+                    </button>
                 </div>
+            </div>
+            <div id="meme_container">
                 
-            </form>
+            </div>
         </main>
     )
 }
